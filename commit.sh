1e4bf992d4ed74bf33960ac722e1854fb0537e07
you <you@example.com>
2023-07-17 11:31:34 +0800

    message

--- commit.sh
+++ commit.sh
@@ -1,3 +1,6 @@
+# for auto commit generation: https://github.com/di-sukharev/opencommit
+# obtain free api key: https://github.com/chatanywhere/GPT_API_free
+
 git pull
 git add .
 git commit -m "message"
