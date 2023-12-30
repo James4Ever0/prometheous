b4b54a321961a8a5e39c633d120fd88b78494cf7
GitJournal <app@gitjournal.io>
2023-12-31 01:51:12 +0800

    message

---
+++ document_agi_computer_control/code_view_with_path_argument_and_anchor/code_view_demo.py
@@ -0,0 +1,64 @@
+print("whoami")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
+
+def greet(name):
+    print("Hello, " + name)
+
+greet("World")
