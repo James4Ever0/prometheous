561253c5e4a732e2aa0bf6282a481ad84a418b79
GitJournal <app@gitjournal.io>
2024-01-02 23:55:56 +0800

    message

--- document_agi_computer_control/path_select_utils.py
+++ document_agi_computer_control/path_select_utils.py
@@ -1,3 +1,3 @@
 # select paths under a directory, for documentation purposes
 # shall be interactive, and work with estimation utils.
-# shall be cached.
+# shall be cached. shall exclude processed files
