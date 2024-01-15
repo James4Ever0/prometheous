79ca4923619bea727afdc6fa9a35eb3fb0474d05
GitJournal <app@gitjournal.io>
2024-01-15 23:14:06 +0800

    update

--- document_agi_computer_control/build_new.sh
+++ document_agi_computer_control/build_new.sh
@@ -6,18 +6,18 @@ export HEADLESS=true
 # # doing small trials repeatedly, for a long time, for many many feedback loops.
 # # then maybe someday it would go automatic.
 
-# bash build_gemini_doc.sh -s 
-# env PROJECT_NAME="graph-of-thoughts" AUTHOR="spcl" bash build_doc_with_author_and_project_name.sh -s
-# env PROJECT_NAME="LLM_Tree_Search" AUTHOR="waterhorse1" bash build_doc_with_author_and_project_name.sh -s
-# bash build_pyjom_doc.sh -s
-# env PROJECT_NAME="DALLE2-pytorch" AUTHOR="lucidrains" bash build_doc_with_author_and_project_name.sh -s
-# env PROJECT_NAME="Kacket" AUTHOR="ShorterThanDijkstra" bash build_doc_with_author_and_project_name.sh -s
-# env PROJECT_NAME="vced" AUTHOR="datawhalechina" bash build_doc_at_prima_with_author_and_project_name.sh -s
-# env PROJECT_NAME="he4o" AUTHOR="jiaxiaogang" bash build_doc_at_prima_with_author_and_project_name.sh -s
-# env PROJECT_NAME="gato" AUTHOR="OrigamiDream" bash build_doc_at_prima_with_author_and_project_name.sh -s
-# env PROJECT_NAME="Voyager" AUTHOR="MineDojo" bash build_doc_at_prima_with_author_and_project_name.sh -s
-# env PROJECT_NAME="RoboGen" AUTHOR="Genesis-Embodied-AI" bash build_doc_at_prima_with_author_and_project_name.sh -s
-# env PROJECT_NAME="mcts-for-llm" AUTHOR="shunzh" bash build_doc_at_prima_with_author_and_project_name.sh -s 
+bash build_gemini_doc.sh -s 
+env PROJECT_NAME="graph-of-thoughts" AUTHOR="spcl" bash build_doc_with_author_and_project_name.sh -s
+env PROJECT_NAME="LLM_Tree_Search" AUTHOR="waterhorse1" bash build_doc_with_author_and_project_name.sh -s
+bash build_pyjom_doc.sh -s
+env PROJECT_NAME="DALLE2-pytorch" AUTHOR="lucidrains" bash build_doc_with_author_and_project_name.sh -s
+env PROJECT_NAME="Kacket" AUTHOR="ShorterThanDijkstra" bash build_doc_with_author_and_project_name.sh -s
+env PROJECT_NAME="vced" AUTHOR="datawhalechina" bash build_doc_at_prima_with_author_and_project_name.sh -s
+env PROJECT_NAME="he4o" AUTHOR="jiaxiaogang" bash build_doc_at_prima_with_author_and_project_name.sh -s
+env PROJECT_NAME="gato" AUTHOR="OrigamiDream" bash build_doc_at_prima_with_author_and_project_name.sh -s
+env PROJECT_NAME="Voyager" AUTHOR="MineDojo" bash build_doc_at_prima_with_author_and_project_name.sh -s
+env PROJECT_NAME="RoboGen" AUTHOR="Genesis-Embodied-AI" bash build_doc_at_prima_with_author_and_project_name.sh -s
+env PROJECT_NAME="mcts-for-llm" AUTHOR="shunzh" bash build_doc_at_prima_with_author_and_project_name.sh -s 
 
 env PROJECT_NAME="lmql-tree-of-thoughts" AUTHOR="LachlanGray" bash build_doc_at_prima_with_author_and_project_name.sh -s
 env PROJECT_NAME="RT-X" AUTHOR="kyegomez" bash build_doc_at_prima_with_author_and_project_name.sh -s
