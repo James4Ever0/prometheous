5e5443bb34bd02d4e28cb238eaaaca9ea942405f
GitJournal <app@gitjournal.io>
2024-01-17 00:11:41 +0800

    message

---
+++ document_agi_computer_control/vectorstore_embedding_chat_rag/README.md
@@ -0,0 +1,3 @@
+how to retrieve documents only related to current file? is that necessary?
+
+is it better to search the whole project?
--- document_agi_computer_control/vectorstore_embedding_chat_rag/docarray_test.py
+++ document_agi_computer_control/vectorstore_embedding_chat_rag/docarray_test.py
@@ -67,6 +67,28 @@ for it in docs:
     docObject = TextDoc(text=it, text_hash=docHash, embedding=embed)
     index.index(docObject)
 
+# breakpoint()
+
+index._sqlite_cursor.execute("SELECT doc_id FROM docs WHERE text LIKE 'hello%'")
+# index._sqlite_cursor.execute("SELECT doc_id FROM docs")
+# [(423764937781332251,), (955323081996155123,)]
+# index._sqlite_cursor.execute("SELECT * FROM docs") # field: doc_id
+rows = index._sqlite_cursor.fetchall()
+# print(rows)
+hashed_ids = set(it[0] for it in rows)
+# hashed_ids = set(str(it[0]) for it in rows)
+# print(hashed_ids)
+ans = index._search_and_filter(
+    np.array(ollama_emb.embed_query(query)).reshape(1, -1),
+    limit=10,
+    search_field="embedding",
+    hashed_ids=hashed_ids,
+)
+rich.print("ans:", ans)
+# breakpoint()
+
+# hnswlib ids: [955323081996155123, 423764937781332251]
+
 results, scores = index.find(
     ollama_emb.embed_query(query), limit=10, search_field="embedding"
 )
